b55ba32fce97b14da178a9c30051db2115381958
Edem <edemshukurov@amayasoft.uz>
2020-05-30 22:46:58 +0500

    Added "create user" function

--- TechnicalSafetyApplication/Controllers/AdminController.cs
+++ TechnicalSafetyApplication/Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TechnicalSafetyApplication.Models;
+using TechnicalSafetyApplication.Models.ViewModels;
 
 namespace TechnicalSafetyApplication.Controllers
 {
@@ -20,6 +21,40 @@ namespace TechnicalSafetyApplication.Controllers
         public IActionResult Index()
         {
             return View(_userManager.Users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel createViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = createViewModel.Name,
+                    Email = createViewModel.Email
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(user, createViewModel.Password);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View(createViewModel);
         }
     }
 }
