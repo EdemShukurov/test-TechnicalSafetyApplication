05c2f327b5bc5c82f4ebce212d74140cf730a8b5
Edem <edemshukurov@amayasoft.uz>
2020-05-30 11:11:59 +0500

    Enabled the delivery of static content (css, js)

--- TechnicalSafetyApplication/Startup.cs
+++ TechnicalSafetyApplication/Startup.cs
@@ -30,6 +30,8 @@ namespace TechnicalSafetyApplication
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
