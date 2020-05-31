d98752b989b8e04b0b1ad9a3b2f512bde50376cc
Edem <edemshukurov@amayasoft.uz>
2020-05-31 08:58:41 +0500

    Added LogIn function

---
+++ TechnicalSafetyApplication/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TechnicalSafetyApplication.Models;
+using TechnicalSafetyApplication.Models.ViewModels;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace TechnicalSafetyApplication.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<AppUser> _userManager;
+        private SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel details, string returnUrl)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(details.Email);
+                if(user != null)
+                {
+                    await _signInManager.SignOutAsync();
+
+                    SignInResult result = await _signInManager.PasswordSignInAsync(user, details.Password, false, false);
+
+                    if(result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+
+                ModelState.AddModelError(nameof(LoginViewModel.Email), "Invalid User or password");
+            }
+
+            return View(details);
+        }
+    }
+}
---
+++ TechnicalSafetyApplication/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechnicalSafetyApplication.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [UIHint("email")]
+        public string Email { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+    }
+}
